0d1fb83cd84cf69e321ca814a5c933e6a27c882f
GeorgeMugurel <fluerasugeorgemugurel@gmail.com>
2018-12-15 12:48:36 +0200

    spin

--- src/app/my-user/my-user.component.ts
+++ src/app/my-user/my-user.component.ts
@@ -12,6 +12,8 @@ export class MyUserComponent implements OnInit {
   myUsers: User[];
   restApi: RestApiService;
 
+  loadingSpiner: boolean;
+
   constructor(restApi : RestApiService) {
 
     this.restApi = restApi;
@@ -43,22 +45,27 @@ export class MyUserComponent implements OnInit {
   }
 
   getUsers(): any {
+    this.loadingSpiner = true;
     this.restApi.getAll().subscribe(response => {
+      this.loadingSpiner = false;
       console.log(response);
       this.myUsers = response;
     })
   }
 
   addUser(user:User) {
-    // this.myUsers.push(user);
+    this.loadingSpiner = true;
     this.restApi.saveUser(user).subscribe(response=>{
+      this.loadingSpiner = false;
       console.log(response);
       this.getUsers();
     });
   }
 
   deleteUser(user:User) {
+    this.loadingSpiner = true;
     this.restApi.restDelete(user._id).subscribe(response=>{
+      this.loadingSpiner = false;
       console.log(response);
       this.getUsers();
     });
